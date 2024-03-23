b7eccc265b95a134409e5bf1f84baf9b88805ef2
supersquad <supersquad@supersquadui-MacBookPro.local>
2024-03-24 01:39:28 +0900

    feat:

--- supersquad-farcaster/src/app/page.tsx
+++ supersquad-farcaster/src/app/page.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
 
-const Index = () => {
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Vote"
+    }
+  ],
+  image: "https://bronze-useful-hummingbird-283.mypinata.cloud/ipfs/QmaxBNrFVZvxAXog5oCc7x1Myp9BtXnHPPfi9WkffNB6t5",
+  post_url: "http://localhost:3001/vote/0x5D83d501c3f6c50fE9891af3486d16A3D29e243F"
+});
+
+export const metadata: Metadata = {
+  title: 'Vote.Fi',
+  description: "Welcome To Vote.Fi. Get reward through Vote!",
+  openGraph: {
+    title: 'Vote.Fi',
+    description: "Welcome To Vote.Fi. Get reward through Vote!",
+    images: ["https://bronze-useful-hummingbird-283.mypinata.cloud/ipfs/QmaxBNrFVZvxAXog5oCc7x1Myp9BtXnHPPfi9WkffNB6t5"]
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
   return (
-    <div>
-      Hello, World
-    </div>
-  )
+    <>
+      <h1>Vote.Fi.</h1>
+    </>
+  );
 }
-
-export default Index
