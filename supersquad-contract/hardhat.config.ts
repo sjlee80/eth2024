0c7228d066c51a5599af1e08c3d8aee8e64b5d51
sjlee80 <sjlee80@gmail.com>
2024-03-23 23:24:49 +0900

    Fix: network info and deploy

--- supersquad-contract/hardhat.config.ts
+++ supersquad-contract/hardhat.config.ts
@@ -1,17 +1,42 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ACCOUNT, INFURA_API_KEY } = process.env;
+
+if (!ACCOUNT) {
+  throw new Error('The ACCOUNT environment variable is not defined');
+}
+
+if (!INFURA_API_KEY) {
+  throw new Error('The INFURA_API_KEY environment variable is not defined');
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: '0.8.24',
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${"a56f310dd02346b098e9732b5343b0c8"}`,
-      accounts: ["82464dbafe8d0199db36b7874a9325cecc569863b54169b6ae80a36cc6b44a76"],
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ACCOUNT],
     },
     baseSepolia: {
       url: 'https://sepolia.base.org',
-      accounts: ["82464dbafe8d0199db36b7874a9325cecc569863b54169b6ae80a36cc6b44a76"],
-    }
+      accounts: [ACCOUNT],
+    },
+    cardonaPolygon: {
+      url: 'https://rpc.cardona.zkevm-rpc.com',
+      accounts: [ACCOUNT],
+    },
+    scrollSepolia: {
+      url: 'https://sepolia-rpc.scroll.io/',
+      accounts: [ACCOUNT],
+    },
+    lineaGoerli: {
+      url: 'https://rpc.goerli.linea.build',
+      accounts: [ACCOUNT],
+    },
   },
 };
 
