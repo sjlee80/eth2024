4cf80c69155a001633aadb30524db78f78fec74d
supersquad <supersquad@supersquadui-MacBookPro.local>
2024-03-24 01:11:34 +0900

    fix: image path

--- supersquad-farcaster/src/app/api/frame/route.ts
+++ supersquad-farcaster/src/app/api/frame/route.ts
@@ -1,11 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-
+  
   return new NextResponse(`<!DOCTYPE html><html><head>
     <title>Hello, World</title>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmaxBNrFVZvxAXog5oCc7x1Myp9BtXnHPPfi9WkffNB6t5" />
+    <meta property="fc:frame:image" content="https://bronze-useful-hummingbird-283.mypinata.cloud/ipfs/QmaxBNrFVZvxAXog5oCc7x1Myp9BtXnHPPfi9WkffNB6t5" />
     <meta property="fc:frame:button:1" content="Vote" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
     <meta property="fc:frame:button:2" content="Mint" />
